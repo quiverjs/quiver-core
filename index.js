92f14ba5111e0d511888115804a1fb16d5bd35b6
Soares Chen <soares.chen@gmail.com>
2016-07-02 19:04:24 +0800

    Change compilation dir to dist/.

--- index.js
+++ index.js
@@ -1,2 +1,2 @@
 'use strict'
-module.exports = require('./out/lib')
+module.exports = require('./dist/lib')
--- test.js
+++ test.js
@@ -1,2 +1,2 @@
 'use strict'
-module.exports = require('./out/test')
+module.exports = require('./dist/test')
